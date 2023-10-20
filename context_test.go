d1cf8b5b247ec9a3e8cb001238e0f8970de1e296
Log-Hori'xins <133627973+Nur-Hidayat-FTI22E@users.noreply.github.com>
2023-10-20 13:14:28 +0000

    contextt

--- context_test.go
+++ context_test.go
@@ -13,3 +13,23 @@ func TestContext(t *testing.T)  {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+	contextF := context.WithValue(contextC, "f", "F")
+
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+}
