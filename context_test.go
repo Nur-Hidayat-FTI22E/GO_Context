cde6f539fda1ba9970d3121dd99e8a56bccca75f
Log-Hori'xins <133627973+Nur-Hidayat-FTI22E@users.noreply.github.com>
2023-10-22 16:28:01 +0000

    context

--- context_test.go
+++ context_test.go
@@ -3,6 +3,7 @@ package gocontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -31,5 +32,40 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextD)
 	fmt.Println(contextE)
 	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+}
+
+func CreateCounter() chan int  {
+	destination := make(chan int)
+
+	go func()  {
+		defer close(destination)
+		counter := 1
+
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T)  {
+	fmt.Println("Total goroutine", runtime.NumGoroutine())	
+
+	destination := CreateCounter()
+
+	for n := range destination{
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	fmt.Println("Total goroutine", runtime.NumGoroutine())	
 
 }
